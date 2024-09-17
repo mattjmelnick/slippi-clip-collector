20605e130950131415ca4156aa48df047d566c72
mattjmelnick <49130549+mattjmelnick@users.noreply.github.com>
2024-09-16 23:31:17 -0400

    Add buttons and placeholder labels

--- slippi-clip-collector/Form1.Designer.cs
+++ slippi-clip-collector/Form1.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(44, 58);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 66);
+            button1.TabIndex = 0;
+            button1.Text = "Select Replays Folder";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(44, 177);
+            button2.Name = "button2";
+            button2.Size = new Size(173, 66);
+            button2.TabIndex = 1;
+            button2.Text = "Select Clips Folder";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(116, 302);
+            button3.Name = "button3";
+            button3.Size = new Size(137, 53);
+            button3.TabIndex = 2;
+            button3.Text = "Save";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(436, 302);
+            button4.Name = "button4";
+            button4.Size = new Size(137, 53);
+            button4.TabIndex = 3;
+            button4.Text = "Delete";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(233, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(182, 28);
+            label1.TabIndex = 4;
+            label1.Text = "Replays Folder Path";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(233, 193);
+            label2.Name = "label2";
+            label2.Size = new Size(158, 28);
+            label2.TabIndex = 4;
+            label2.Text = "Clips Folder Path";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(718, 440);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Slippi Clip Collector";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
     }
 }
--- slippi-clip-collector/Form1.cs
+++ slippi-clip-collector/Form1.cs
@@ -6,5 +6,35 @@ namespace slippi_clip_collector
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
